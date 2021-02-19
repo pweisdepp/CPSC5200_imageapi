a650fa123bd005bf354ae3b0f26bb51a5d9e436c
pete <weisdepp@seattleu.edu>
2021-02-18 22:13:35 -0800

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,84 @@
+
+#![feature(decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+extern crate rocket_multipart_form_data;
+
+use rocket::http::ContentType;
+use rocket::Data;
+
+use rocket_multipart_form_data::mime;
+use rocket_multipart_form_data::{
+    MultipartFormData, MultipartFormDataError, MultipartFormDataField, MultipartFormDataOptions,
+};
+
+#[macro_use]
+use rocket_include_static_resources::static_response;
+use rocket_include_static_resources::static_resources_initialize;
+
+use rocket_include_static_resources::StaticResponse;
+use rocket_raw_response::RawResponse;
+
+
+#[get("/")]
+fn index() -> StaticResponse {
+    static_response!("html-image-uploader")
+}
+
+#[post("/upload", data = "<data>")]
+fn upload(content_type: &ContentType, data: Data) -> Result<RawResponse, &'static str> {
+    let options = MultipartFormDataOptions::with_multipart_form_data_fields(vec![
+        MultipartFormDataField::raw("image")
+            .size_limit(32 * 1024 * 1024)
+            .content_type_by_string(Some(mime::IMAGE_STAR))
+            .unwrap(),
+    ]);
+
+    let mut multipart_form_data = match MultipartFormData::parse(content_type, data, options) {
+        Ok(multipart_form_data) => multipart_form_data,
+        Err(err) => {
+            match err {
+                MultipartFormDataError::DataTooLargeError(_) => {
+                    return Err("The file is too large.");
+                }
+                MultipartFormDataError::DataTypeError(_) => {
+                    return Err("The file is not an image.");
+                }
+                _ => panic!("{:?}", err),
+            }
+        }
+    };
+
+    let image = multipart_form_data.raw.remove("image");
+
+    match image {
+        Some(mut image) => {
+            let raw = image.remove(0);
+
+
+            // Image processing begins
+            let mut image =
+            let content_type = raw.content_type;
+            let file_name = raw.file_name.unwrap_or("Image".to_string());
+            let data = raw.raw;
+
+            Ok(RawResponse::from_vec(data, Some(file_name), content_type))
+        }
+        None => Err("Please input a file."),
+    }
+}
+
+fn main() {
+    rocket::ignite()
+        .attach(StaticResponse::fairing(|resources| {
+            static_resources_initialize!(
+                resources,
+                "html-image-uploader",
+                "/home/pete/CLionProjects/CPSC5200_imageapi/src/form.html"
+            );
+        }))
+        .mount("/", routes![index])
+        .mount("/", routes![upload])
+        .launch();
+}
